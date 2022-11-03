de47784aed9cf3cbd43e5a02475c097ae8ab8e84
jeseong <mywptjd0127@naver.com>
2022-11-03 16:31:00 +0900

    Feat: 일주일 신고 개수 불러오기

--- src/main/java/com/example/test/domain/service/MemberService.java
+++ src/main/java/com/example/test/domain/service/MemberService.java
@@ -1,9 +1,15 @@
 package com.example.test.domain.service;
 
 import com.example.test.application.response.MemberReportCountResponse;
+import com.example.test.application.response.MemberWeekReportCountResponse;
 import com.example.test.domain.model.dto.GetMemberReportsDto;
+import com.example.test.domain.model.dto.GetMemberWeekReportCountDto;
 import com.example.test.domain.model.repository.ReportRepository;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -24,5 +30,21 @@ public class MemberService {
 
     public MemberReportCountResponse getMemberReportCount(String memberId) {
         return new MemberReportCountResponse(reportRepository.countByRegId(memberId), reportRepository.countByRegIdAndStatus(memberId, 1));
+    }
+
+    public MemberWeekReportCountResponse getMemberWeekReportCount(String memberId) {
+        List<GetMemberWeekReportCountDto> reports = new ArrayList<>();
+        LocalDate now = LocalDate.now();
+
+        for (int i = 0; i < 7; i++) {
+            LocalDateTime start = LocalDateTime.of(
+                    now.minusDays(i), LocalTime.of(0, 0, 0));
+            LocalDateTime end = LocalDateTime.of(
+                    now.minusDays(i), LocalTime.of(23, 59, 59));
+
+            reports.add(new GetMemberWeekReportCountDto(start.getDayOfWeek(), reportRepository.countByRegIdAndRegDateBetween(memberId, start, end)));
+        }
+
+        return new MemberWeekReportCountResponse(reports);
     }
 }
