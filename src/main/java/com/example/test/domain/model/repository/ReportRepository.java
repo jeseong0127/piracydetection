b10b1806cc5190bd1894425070669a49d78a317e
jeseong <mywptjd0127@naver.com>
2022-11-03 13:43:15 +0900

    Fix: 내 신고리스트 가져오기 조건 수정

--- src/main/java/com/example/test/domain/model/repository/ReportRepository.java
+++ src/main/java/com/example/test/domain/model/repository/ReportRepository.java
@@ -6,13 +6,14 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReportRepository extends JpaRepository<Report, Integer> {
 
-    @Query("select r from Report r join fetch r.metadata")
-    List<Report> findAllByRegId(String memberId);
+    @Query("select r from Report r join fetch r.metadata where r.regId = :memberId")
+    List<Report> findAllByRegId(@Param("memberId") String memberId);
 
     int countByRegId(String memberId);
 
