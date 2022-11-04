f3c43d7f1f17c66469ffd2b9ff6f4569d1be7e9b
jeseong <mywptjd0127@naver.com>
2022-11-04 14:52:17 +0900

    Feat: ai로 검색한 이미지 조회

--- src/main/java/com/example/test/application/controller/ImageController.java
+++ src/main/java/com/example/test/application/controller/ImageController.java
@@ -9,6 +9,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +27,14 @@ public class ImageController {
             @PathVariable int imageId
     ) {
         return imageService.viewImage(imageId);
+    }
+
+    @ApiOperation(value = "검색 대표 이미지 조회하기")
+    @GetMapping(produces = MediaType.IMAGE_JPEG_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public byte[] viewThumbnailImage(
+            @RequestParam String imagePath
+    ) {
+        return imageService.viewThumbnailImage(imagePath);
     }
 }
